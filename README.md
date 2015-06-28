579a393dc82ce8f8643b0b454b085a567ed650e4
Mike Bild <mike@mikebild.com>
2015-06-28 12:04:47 +0200

    Add graceful shutdown on SIGTERM

--- README.md
+++ README.md
@@ -7,8 +7,9 @@ export SERVICE_PORT=5008
 export CART_TIMEOUT=60000
 export RETRY_TIMEOUT=5000
 export DISCOVERY_SERVICE_URLS=http://46.101.138.192:8500,http://46.101.191.124:8500
-export MAX_REQUEST_PER_SECOND=2
+export MAX_REQUEST_PER_MINUTE=5
 export REQUEST_THROTTLE_MS=10
+export SHUTDOWN_TIMEOUT_MS=10000
 ```
 
 ##Build
@@ -22,8 +23,8 @@ export REQUEST_THROTTLE_MS=10
 ##Release into private registry
 
 ```
-docker tag cart-service 46.101.191.124:5000/cart-service:0.0.9
-docker push 46.101.191.124:5000/cart-service:0.0.9
+docker tag cart-service 46.101.191.124:5000/cart-service:0.0.14
+docker push 46.101.191.124:5000/cart-service:0.0.14
 ```
 
 ##Deploy via Shipyard
@@ -34,17 +35,18 @@ curl -X POST \
 -H 'X-Service-Key: pdE4.JVg43HyxCEMWvsFvu6bdFV7LwA7YPii' \
 http://46.101.191.124:8080/api/containers?pull=true \
 -d '{  
-  "name":"46.101.191.124:5000/cart-service:0.0.9",
+  "name":"46.101.191.124:5000/cart-service:0.0.14",
   "cpus":0.1,
-  "memory":64,
+  "memory":32,
   "environment":{
     "SERVICE_CHECK_SCRIPT":"curl -s http://46.101.191.124:5020/healthcheck",
     "DISCOVERY_SERVICE_URLS":"http://46.101.138.192:8500,http://46.101.191.124:8500",
     "SERVICE_PORT":"5020",
     "CART_TIMEOUT":"3600000",
     "RETRY_TIMEOUT":"5000",
-    "MAX_REQUEST_PER_SECOND":"5",
+    "MAX_REQUEST_PER_MINUTE":"75",
     "REQUEST_THROTTLE_MS":"10",
+    "SHUTDOWN_TIMEOUT_MS":"10000",
     "LOG":"true"
   },
   "hostname":"",
--- serve.js
+++ serve.js
@@ -14,24 +14,25 @@ var serviceSDK      = require('lc-sdk-node.js');
 var DISCOVERY_SERVICE_URLS = (process.env.DISCOVERY_SERVICE_URLS || '').split(/\s*;\s*|\s*,\s*/);
 var RETRY_TIMEOUT =  parseInt(process.env.RETRY_TIMEOUT) || 2000;
 var CART_TIMEOUT = parseInt(process.env.CART_TIMEOUT) || 60000;
-var MAX_REQUEST_PER_SECOND = parseInt(process.env.MAX_REQUEST_PER_SECOND) || 5;
+var MAX_REQUEST_PER_MINUTE = parseInt(process.env.MAX_REQUEST_PER_MINUTE) || 5;
 var REQUEST_THROTTLE_MS = parseInt(process.env.REQUEST_THROTTLE_MS) || 10;
+var SHUTDOWN_TIMEOUT_MS = parseInt(process.env.SHUTDOWN_TIMEOUT_MS) || 10000;
 
 var serviceClient = serviceSDK({ discoveryServers: DISCOVERY_SERVICE_URLS });
 
-var server = express();
-server.use(responseTime(function(req, res, time){
+var app = express();
+app.use(responseTime(function(req, res, time){
   console.log('LOG: ' + req.url + ',' + res.statusCode + ',' + time);
 }));
-server.enable('trust proxy');
-server.use(rateLimit({
-        windowMs: 1000,
+app.enable('trust proxy');
+app.use(rateLimit({
+        windowMs: 60000,
         delayMs: REQUEST_THROTTLE_MS,
-        max: MAX_REQUEST_PER_SECOND,
+        max: MAX_REQUEST_PER_MINUTE,
         global: true
 }));
-server.use(bodyParser.json());
-server.use(function(req, res, next){
+app.use(bodyParser.json());
+app.use(function(req, res, next){
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, DELETE, PUT');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -49,16 +50,11 @@ var catalogServiceBreaker = new CircuitBreaker({
 
 retryReplicate();
 
-server.get('/healthcheck', function(req, res){
-  res.send({ message: 'OK', version: app.version});
-});
-
-server.get('/cart/:key?', function(req, res){
+app.get('/cart/:key', function(req, res){
+  if(!req.params.key) return res.send(400, {message: 'cart id is missing'});
 
   var cart = cache.get(req.params.key);
-
-  if(!cart) cart = {};
-  if(!cart.orders) cart.orders = [];
+  if(!cart) return res.send(404, {message: 'cart not found'});
   
   cart.total = 0;
   cart.datetime = new Date();
@@ -107,17 +103,17 @@ server.get('/cart/:key?', function(req, res){
     cart.cart.total += cart.cart.orders[idx].total || 0;
   }
 
-  res.send(cart);  
+  res.send(200, cart);  
 });
-
-
-server.post('/cart/:key/close', function(req,res){
+app.post('/cart/:key/close', function(req,res){
   var cartId = req.params.key;
-  cache.del(cartId);
-  res.send({id: cartId, message: 'accepted'});
-});
 
-server.post('/cart/:key?', function(req, res){
+  if(!cartId) return res.send(400, {message: 'cart id is missing'});
+
+  cache.del(cartId);
+  res.send(201, {id: cartId, message: 'accepted'});
+});
+app.post('/cart/:key?', function(req, res){
   var cartId = req.params.key || uuid.v4();
   var orders  = req.body || [];
 
@@ -127,11 +123,14 @@ server.post('/cart/:key?', function(req, res){
     datetime: new Date()
   };
 
-  cache.put(cartId, payload, CART_TIMEOUT || 3600000);
-  res.send({id: cartId, message: 'accepted'});
+  cache.put(cartId, payload, CART_TIMEOUT);
+  res.send(201, {id: cartId, message: 'accepted'});
 });
 
-server.get('/replicate', function(req, res){
+app.get('/healthcheck', function(req, res){
+  res.send({ message: 'OK', version: app.version});
+});
+app.get('/replicate', function(req, res){
   var command = function(success, failed) {
     replicate(function(result){
        res.send({message: result.body.rows.length + ' items replicated'});
@@ -143,15 +142,31 @@ server.get('/replicate', function(req, res){
     res.send({ message: 'Replication error' });
   });
 });
-
-server.get('/error', function(req, res){
+app.get('/error', function(req, res){
   console.log(new Error('Error - shut down'));
   process.exit(-1);
 });
 
-server.listen(process.env.SERVICE_PORT, function(){
+var server = app.listen(process.env.SERVICE_PORT, function(){
   console.log('Listen on ' + process.env.SERVICE_PORT);
 });
+
+process.on('SIGTERM', gracefulShutdown);
+process.on('SIGINT', gracefulShutdown);
+
+function gracefulShutdown() {
+  console.log('Received kill signal, shutting down gracefully in ' + SHUTDOWN_TIMEOUT_MS + 'ms');
+  server.close(function() {
+    console.log('Closed out remaining connections');
+    console.error('Shutting down after ' + SHUTDOWN_TIMEOUT_MS + 'ms');
+    setTimeout(process.exit, SHUTDOWN_TIMEOUT_MS);
+  });
+
+  setTimeout(function() {
+   console.error('Force shutting down after ' + (SHUTDOWN_TIMEOUT_MS * 3) + 'ms');
+   process.exit();
+  }, (SHUTDOWN_TIMEOUT_MS * 3));  
+}
 
 function replicate(success, failed) {
   serviceClient.get('couchdb', '/products/_all_docs?include_docs=true')
